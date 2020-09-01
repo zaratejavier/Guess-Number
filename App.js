37cfbe8d1d635640f480a4ec95096fd92a0062ff
Javier Zarate <zarate.javier.jz@gmail.com>
2020-08-31 21:59:37 -0600

    added a header component with guess a number text and a red background

--- App.js
+++ App.js
@@ -1,15 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import Header from './Components/Header'
 
 export default function App() {
   return (
-    <View>
-
+    <View style={styles.screen}>
+      <Header title={"Guess a number"}/>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  
+  screen: {
+    // flex: 1
+  }
 });

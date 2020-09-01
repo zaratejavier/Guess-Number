87c4f0994fc4c4816727bd408beffb520c6dacca
Javier Zarate <zarate.javier.jz@gmail.com>
2020-09-01 17:25:18 -0600

    added a screens component

--- App.js
+++ App.js
@@ -13,6 +13,6 @@ export default function App() {
 
 const styles = StyleSheet.create({
   screen: {
-    // flex: 1
+    flex: 1
   }
 });
---
+++ Screens/StartGameScreen.js
@@ -0,0 +1,21 @@
+import React from "react"
+import { View, Text, StyleSheet } from "react-native";
+
+const StartGameScreen = () => {
+  return (
+    <View>
+      
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 10,
+    alignItems: 'center',
+    justifyContent
+  }
+})
+
+export default StartGameScreen

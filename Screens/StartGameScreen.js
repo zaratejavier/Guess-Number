7e953e86c7e13a755a02a620783ca44fdab81862
Javier Zarate <zarate.javier.jz@gmail.com>
2020-09-01 21:37:11 -0600

    added a card component so i can use the styles else where

--- Screens/StartGameScreen.js
+++ Screens/StartGameScreen.js
@@ -1,18 +1,19 @@
 import React from "react"
 import { View, Text, StyleSheet, TextInput, Button } from "react-native";
+import Card from "../Components/Card"
 
 const StartGameScreen = () => {
   return (
     <View style={styles.screen}>
       <Text style={styles.title}>Start a new Game!</Text>
-      <View style={styles.inputContainer}>
+      <Card style={styles.inputContainer}>
         <Text>Select a number</Text>
           <TextInput />
           <View style={styles.buttonContainer}>
             <Button title="Reset" onPress={() => { }} />
             <Button title="Confirm" onPress={() => { }}/>
           </View>
-      </View>
+      </Card>
     </View>
   )
 }
@@ -31,13 +32,7 @@ const styles = StyleSheet.create({
   inputContainer: {
     width: 300,
     maxWidth: '80%',
-    alignItems: 'center',
-    shadowColor: 'black',
-    shadowOffset: { width: 0, height: 2 },
-    shadowRadius: 6,
-    shadowOpacity: 0.26,
-    backgroundColor: 'white',
-
+    alignItems: 'center'
   },
   buttonContainer: {
     flexDirection: 'row',

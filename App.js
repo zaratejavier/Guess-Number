0fcd879f3c6ed06d3841a5a1d21b2fd6f91a4014
Javier Zarate <zarate.javier.jz@gmail.com>
2020-09-01 21:11:29 -0600

    added button and style to the startGameScreen

--- App.js
+++ App.js
@@ -2,11 +2,13 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import Header from './Components/Header'
+import StartGameScreen from "./Screens/StartGameScreen"
 
 export default function App() {
   return (
     <View style={styles.screen}>
-      <Header title={"Guess a number"}/>
+      <Header title={"Guess a number"} />
+      <StartGameScreen/>
     </View>
   );
 }
--- Screens/StartGameScreen.js
+++ Screens/StartGameScreen.js
@@ -1,10 +1,18 @@
 import React from "react"
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, TextInput, Button } from "react-native";
 
 const StartGameScreen = () => {
   return (
-    <View>
-      
+    <View style={styles.screen}>
+      <Text style={styles.title}>Start a new Game!</Text>
+      <View style={styles.inputContainer}>
+        <Text>Select a number</Text>
+          <TextInput />
+          <View style={styles.buttonContainer}>
+            <Button title="Reset" onPress={() => { }} />
+            <Button title="Confirm" onPress={() => { }}/>
+          </View>
+      </View>
     </View>
   )
 }
@@ -14,7 +22,22 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 10,
     alignItems: 'center',
-    justifyContent
+    // justifyContent: "center",
+  },
+  title: {
+    fontSize: 20,
+    marginVertical: 10,
+  },
+  inputContainer: {
+    width: 300,
+    maxWidth: '80%',
+    alignItems: 'center'
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    width: '100%',
+    justifyContent: 'space-between',
+    paddingHorizontal: 15,
   }
 })
 
